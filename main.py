1e8169a15ea2577fbcd175ad2511af3ef28ab5f6
dilmurod <dilmurod@kali.dilmurod>
2025-01-25 10:22:23 -0500

    insert_multiple

--- main.py
+++ main.py
@@ -7,4 +7,13 @@ user = {
     'lastname': 'Elmurodov','name': 'Ziyodullo', 'age': 20 , 'job': 'student'
     }
 
-db.insert(user)
+
+user2 = {'lastname': 'Mamarajabov','name': 'Dilmurod', 'age': 22 , 'job': 'student'}
+
+user3 = {'lastname': 'Odil','name': 'Kamronbek', 'age': 19 , 'job': 'engineer'}
+
+user4 = {'lastname': 'Nuraliyev','name': 'Akobir', 'age': 15 , 'job': 'student'}
+
+user5 = {'lastname': 'Aliyev','name': 'Qodir', 'age': 32 , 'job': 'teacher'}
+
+db.insert_multiple([user,user2,user3,user4,user5])
