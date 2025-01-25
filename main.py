801c5e976a3b27c8d724afacb5ddcb5d5b650ade
dilmurod <dilmurod@kali.dilmurod>
2025-01-25 10:34:50 -0500

    Document

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
 from tinydb import TinyDB
-
+from tinydb.table import Document
 
 db = TinyDB("db.json")
 
-user = {
-    'lastname': 'Elmurodov','name': 'Ziyodullo', 'age': 20 , 'job': 'student'
-    }
+user = Document({
+    'lastname': 'Elmurodov',
+    'name': 'Ziyodullo',
+    'age': 20,
+    'job': 'student'
+}, doc_id=10)
 
-
-user2 = {'lastname': 'Mamarajabov','name': 'Dilmurod', 'age': 22 , 'job': 'student'}
-
-user3 = {'lastname': 'Odil','name': 'Kamronbek', 'age': 19 , 'job': 'engineer'}
-
-user4 = {'lastname': 'Nuraliyev','name': 'Akobir', 'age': 15 , 'job': 'student'}
-
-user5 = {'lastname': 'Aliyev','name': 'Qodir', 'age': 32 , 'job': 'teacher'}
-
-db.insert_multiple([user,user2,user3,user4,user5])
+db.insert(user)
