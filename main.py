0e69d7fdeea9244f69abfd3cf6d304680a9cacc7
dilmurod <dilmurod@kali.dilmurod>
2025-01-28 15:58:34 +0500

    TiynDB

--- main.py
+++ main.py
@@ -2,17 +2,6 @@ from tinydb import TinyDB
 from tinydb.table import Document
 
 db = TinyDB("db.json")
-
-user = Document({
-    'lastname': 'Elmurodov',
-    'name': 'Ziyodullo',
-    'age': 20,
-    'job': 'student'
-}, doc_id=10)
-
-print(db.all())
-
-print(db.contains(doc_id=10))
-print(db.contains(doc_id=10))
-print(db.get(doc_id=10))
-print(db.get(doc_id=1))
+user1 = {"lastname":"Mamarajabov","name":"Dilmurod","age":21,"job":"student"}
+user2 = {"lastnema":"Elmurodov","name":"Ziyodullo","age":20,"job":"student"}
+db.insert_multiple([user1, user2])
