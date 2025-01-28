51e365f62b5f10381cfe60e4b9f0a0e763f11f1f
dilmurod <dilmurod@kali.dilmurod>
2025-01-28 16:32:34 +0500

    shop insert

--- main.py
+++ main.py
@@ -4,8 +4,29 @@ import requests
 
 db = TinyDB("random.json")
 
+shop = TinyDB("shop.json")
 
 
+shopbase = {
+    'samsung': [
+        {'model': 'Galaxy S23 Ultra', 'memory': '512 GB', 'cost': '1.399$'},
+        {'model': 'Galaxy Z Fold 5', 'memory': '1TB', 'cost': '1.799$'},
+        {'model': 'Galaxy A54', 'memory': '128 GB', 'cost': '499$'}
+    ],
+    'iphone': [
+        {'model': 'iPhone 15 Pro Max', 'memory': '1TB', 'cost': '1.599$'},
+        {'model': 'iPhone 14 Pro', 'memory': '256 GB', 'cost': '1.199$'},
+        {'model': 'iPhone SE (2023)', 'memory': '128 GB', 'cost': '599$'}
+    ],
+    'xiaomi': [
+        {'model': 'Redmi Note 13 Pro', 'memory': '256 GB', 'cost': '399$'},
+        {'model': 'Mi 13 Ultra', 'memory': '512 GB', 'cost': '899$'},
+        {'model': 'Poco X5 Pro', 'memory': '128 GB', 'cost': '349$'}
+    ]
+}
+
+
+shop.insert(shopbase)
 
 url ="https://randommer.io/api/Name?nameType=firstname&quantity=10"
 
